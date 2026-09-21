98902266f40d3c47e8b0f36716c6b952aee4b4ea
parthasarathy01 <parthasarathy01@gmail.com>
2026-09-21 12:57:01 +0530

    Add files via upload

--- owner-calendar.js
+++ owner-calendar.js
@@ -36,7 +36,7 @@ const syncErrorDialog = document.querySelector("[data-sync-error-dialog]");
 const syncErrorTitle = document.querySelector("[data-sync-error-title]");
 const syncErrorMessage = document.querySelector("[data-sync-error-message]");
 const googleSheetWebAppUrl =
-  "https://script.google.com/macros/s/AKfycby1NtF5nC63QlLRBnRFSFmdbBw6puYksuSwb-Tm27q7ESwh1sO4QC1bzhX2BB-j9kQJkg/exec";
+  "https://script.google.com/macros/s/AKfycbx1_obb-Sz1auKV4SSjnwJlM_ow9ubtImj1Imn_E3oIruCaANSbTzLRPab9Bz5sfGkygA/exec";
 
 const storageKey = "spss-room-calendar-v2";
 const AC_RATE = 1500;
